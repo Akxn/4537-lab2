846ee328f1d04b6b69fb71a2074a4ce0bcf9bbc8
Akxn <shen.yen112@gmail.com>
2023-01-12 10:09:48 -0800

    commit 2

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ $(document).ready(function () {
       const message = $('#chatbot-input').val();
       $.ajax({
         type: 'POST',
-        url: 'http://localhost:3000/chatbot',
-        // url:,
+        // url: 'http://localhost:3000/chatbot',
+        url: 'https://greenmonkey.onrender.com',
         data: { message: message },
         success: function (response) {
           let newMessage = $('<div>', { class: 'message' }).text(response.text);
